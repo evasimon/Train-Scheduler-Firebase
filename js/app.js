6349ab2e83348dd0e1ca200ae07a7c40765aca53
Eva Simon <eva.simon@gmail.com>
2017-10-04 20:57:45 -0500

    sets auth

--- js/app.js
+++ js/app.js
@@ -11,23 +11,7 @@ $(document).ready(function() {
 	};
 	firebase.initializeApp(config);
 
-	// FirebaseUI config.
-	var uiConfig = {
-		signInSuccessUrl: 'index.html',
-		signInOptions: [
-		// Leave the lines as is for the providers you want to offer your users.
-		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-		firebase.auth.GithubAuthProvider.PROVIDER_ID,
-		],
-		// Terms of service url.
-		tosUrl: '<your-tos-url>'
-	};
-
-	// Initialize the FirebaseUI Widget using Firebase.
-	var ui = new firebaseui.auth.AuthUI(firebase.auth());
-	// The start method will wait until the DOM is loaded.
-	ui.start('#firebaseui-auth-container', uiConfig);
-
+	// checks if the authentication has changed
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
 			// User is signed in.
